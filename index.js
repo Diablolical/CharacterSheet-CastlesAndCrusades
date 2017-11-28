8884855190c72e225dc6e962d982b0fd010f736d
Austin Wilson <austin12w@gmail.com>
2017-11-28 16:07:27 -0600

    Added templating and save/load
    Templates! Saving data! Loading saved data! Stuff!

--- index.js
+++ index.js
@@ -1,38 +1,77 @@
 var express = require("express");
+var parser = require('body-parser');
+var exphbs  = require('express-handlebars');
 var app = express();
 var server = require('http').Server(app);
 var path = require('path');
 var fs = require('fs');
 var conf = require('./config/conf.json');
 
-
+/*Setup*/
 app.set('port', (process.env.PORT) || 5000);
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+/*Serve from public directory*/
 app.use(express.static(path.join(__dirname, 'public/')));
+/*submitted data will be parsed as JSON */
+app.use(parser.json());
+/*Static resources from node_modules */
 app.use('/vue', express.static(path.join(__dirname, 'node_modules/vue/dist/')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
-var characterData = conf.defaultData;
-
+/*Request Handling*/
 app.param('id', (req, res, next, id) => {
 	var path = "tmp/"+id;
 	if (fs.existsSync(path)) {
 		fs.readFile(path, 'utf8', (err, data) => {
-			if(err) throw err;
-			characterData = JSON.parse(data);
-			console.log(characterData);
-			next();
+			if (err) {
+				console.log("Error getting file at path "+path, err);
+				return next(err);
+			}
+			if (data) {
+				req.character = JSON.parse(data);
+				return next();
+			}
 		});
 	}
 });
 
 app.get('/:id', (req, res) => {
-	res.sendFile(path.join(__dirname, 'views/sheet.html'))
+	var characterData = req.character;
+	res.render('castlesandcrusades', {
+        data: JSON.stringify(characterData)
+    });
 });
 
-app.post('/', function (req, res) {
-  res.send('POST request received');
-  //do actual stuff
+app.get('/', (req, res) => {
+	var characterData = conf.defaultData;
+	res.render('castlesandcrusades', {
+		data: JSON.stringify(characterData)
+	});
+});
+/*literally just write JSONs to flat files for now. Add a proper DB at some point if deemed necessary */
+app.post('/', (req, res) => {
+	console.log(data);
+	//using name as ID for now
+	var id = data.general.name;
+	var path = "tmp/"+id;
+	var dataToWrite = JSON.stringify(data);
+	fs.writeFile(path, dataToWrite, (err) => {
+		if(err) {
+			var retObj = {
+				"success": 0,
+				"message": "Unable to save data",
+				"details": err.toString()
+			};
+			res.send(JSON.stringify(retObj));
+			return console.log(err);
+		}
+		//redirect to url of their saved character
+		res.redirect('/'+id);
+		console.log("Saved data");
+	});
 });
 
+/*Init*/
 server.listen(app.get('port'), () => console.log("Server initiated and listening on port: " + app.get('port')));
--- public/js/sheet.js
+++ public/js/sheet.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var character =  {
+/*var character =  {
     general: {
         name: "",
         title: "",
@@ -62,10 +62,77 @@ var character =  {
     armor: [
         {type: "", ac: 0}
     ]
-};
+};*/
 /*Vue.component('ability-modifier', {
     template : '<span><input type="text" name="con-modifier" v-bind:value="conMod" readonly="readonly"><label>Ability Modifier</label></span>',
 }); */
+if(typeof character !== "object")
+{
+    alert("Unable to load character data from server");
+    var character =  {
+        general: {
+            name: "",
+            title: "",
+            race: "",
+            class: "",
+            alignment: "",
+            deity: "",
+            level: 0
+        },
+        physical: {
+            age: "",
+            gender: "",
+            hair: "",
+            eyes: "",
+            height: "",
+            weight: "",
+            clothing: "",
+            other: ""
+        },
+        characterization: {
+            languages: "",
+            description: "",
+            personality: "",
+            background: ""
+        },
+        attributes: {
+            str: 0,
+            dex: 0,
+            con: 0,
+            int: 0,
+            wis: 0,
+            cha: 0
+        },
+        modifiers: {
+            strMod: "",
+            dexMod: "",
+            conMod: "",
+            intMod: "",
+            wisMod: "",
+            chaMod: ""
+        },
+        level: {
+            level: 0,
+            experience: 0
+        },
+        defense: {
+            armorAC: "",
+            shieldAC: "",
+            miscAC: "",
+            HP: ""
+        },
+        offense: {
+            miscToHit: ""
+        },
+        abilities: [],
+        weapons: [
+            {type: "", bonusToHit: 0, bonusDamage: 0, damage: 0, special: ""}
+        ],
+        armor: [
+            {type: "", ac: 0}
+        ]
+    };
+}
 
 var app = new Vue({
     el: '#sheet',
@@ -122,7 +189,6 @@ var app = new Vue({
                 alert("Unable to load rules!");
                 return;
             }
-            console.log(val);
             if (val <= 0) {
                 return 0;
             }
@@ -131,22 +197,28 @@ var app = new Vue({
             }
             var levels = rules.modifierLevels;
             var modifier = 0;
-            console.log(levels);
             Object.keys(levels).some(function (score){
-                console.log(score);
                 if (val >= levels[score].min && val <= levels[score].max){
                     modifier = score;
                     return;
                 }
             });
-            console.log(this.attributes);
             return modifier;
         },
         save: function() {
-            this.$http.post('/', JSON.stringify(this.data), {
-                headers: {
-                   'Content-Type': 'application/json'
-               }
+            var data = JSON.stringify(character);
+            console.log(data);
+            $.when($.ajax({
+                    method: 'POST',
+                    url: '/',
+                    data: data,
+                    contentType: "application/json"
+            }))
+            .done(function(data){
+                console.log("Post success with data:",data);
+            })
+            .fail(function(data){
+                console.log("Post failed with data: ",data);
             });
         }
     }
